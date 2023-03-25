e11f83cc3b4549980e9c751cd1272e2ea531b275
mikhailchaly <mikhailchaly@inbox.ru>
2023-03-25 22:42:26 +0300

    add_files

---
+++ first_project/app/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
+import datetime
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+    
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+
+    current_time = datetime.datetime.now().time().strftime("%Hч %Mм %Sс")
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+
+    try:
+        list_fails = os.listdir(path='.')
+        return HttpResponse([list_fails])
+    except:
+        raise NotImplemented
